944a3e0a790d635730e069202d37a610772729f0
张梁 <1071291368@qq.com>
2026-06-26 15:10:15 +0800

    Fix timezone defaults and add technical doc

--- src/main/java/com/hwz/LabCoreApplication.java
+++ src/main/java/com/hwz/LabCoreApplication.java
@@ -3,10 +3,16 @@ package com.hwz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.ZoneId;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class LabCoreApplication {
 
+    public static final ZoneId APPLICATION_ZONE_ID = ZoneId.of("Asia/Shanghai");
+
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone(APPLICATION_ZONE_ID));
         SpringApplication.run(LabCoreApplication.class, args);
     }
 
